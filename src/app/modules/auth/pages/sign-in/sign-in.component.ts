6a6abdd16ce53302faae9e81cbfc218c6b69efa7
OmarOlate <omarolate.96@gmail.com>
2025-05-06 19:47:45 -0400

    feat(login): login implement

--- src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -1,54 +1,90 @@
 import { NgClass, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { AlertComponent, ButtonComponent, IconComponent } from 'src/ui';
 import { emailFormatValidator } from '../custom-validators';
+import { AuthService } from '../data-access';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { AccessTokenDto, UserDataDto } from '../dtos';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   imports: [FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, ButtonComponent, NgClass, IconComponent, AlertComponent],
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
+  readonly #httpService = inject(AuthService);
   hide = true;
-  form!: FormGroup;
   submitted = false;
   passwordTextType!: boolean;
+  errorMessage: string = '';
 
-  constructor(private readonly _formBuilder: FormBuilder, private readonly _router: Router) {}
+  constructor(
+    private readonly _router: Router,
+    ) {}
 
-  ngOnInit(): void {
-    this.form = this._formBuilder.group({
+  protected readonly $hasError = toSignal(this.#httpService.hasError$);
+  protected readonly $isLoading = toSignal(this.#httpService.isLoading$);
+
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group(
+    { 
       email: ['', [Validators.required]],
-      password: ['', Validators.required],
-    }, {
-      Validators:[
-        emailFormatValidator('email')
+      password: ['', [Validators.required]],
+      
+    },{
+      validators: [
+        emailFormatValidator('email'),
       ]
-    });
-  }
+    }
+  )
 
   get f() {
-    return this.form.controls;
+    return this.loginForm.controls;
   }
 
   togglePasswordTextType() {
     this.passwordTextType = !this.passwordTextType;
   }
 
-  submitForm() {
-    this.submitted = true;
-    const { email, password } = this.form.value;
+  loginUser() {
+    console.log('loginUser llamado');
 
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
+    this.submitted = true;
+    const email = this.loginForm.get('email')!.value!;
+    const password = this.loginForm.get('password')!.value!;
+    
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
       return;
     }
-    console.log('Formulario válido:', this.form.value);
 
 
-    this._router.navigate(['/']);
+    this.#httpService
+      .login({ email, password })
+      .pipe(
+        tap(
+          (result: {
+            token: AccessTokenDto['token'];
+            userData: UserDataDto;
+          }) => {
+            if (result.token && result.userData) {
+              localStorage.setItem('token', result.token);
+              localStorage.setItem('userData', JSON.stringify(result.userData));
+              this._router.navigateByUrl('/dashboard/admin');
+            }
+          }
+        ),
+        catchError((error) => {
+          console.error(`Ocurrió un error ${error}`);
+          this.errorMessage = 'Error al iniciar sesión';
+          return of(null);
+        })
+      )
+      .subscribe();
   }
 }
